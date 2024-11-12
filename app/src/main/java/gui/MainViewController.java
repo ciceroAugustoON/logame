40750738965e1be308ca3d5ad045fbce8a030fb6
Estagiario SCE <estagiario.sce@semob.local>
2024-11-12 16:25:52 -0300

    feature - Game list as components

--- app/src/main/java/gui/MainViewController.java
+++ app/src/main/java/gui/MainViewController.java
@@ -10,7 +10,15 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
 import logame.entities.enumerations.LogState;
 import logame.model.dao.Dao;
 import logame.model.dao.DaoFactory;
@@ -18,15 +26,36 @@ import logame.model.entities.Game;
 
 public class MainViewController implements Initializable{
 	private Dao<Game> gameDao;
-	
+	@FXML
+	private VBox mainVBox;
 	@FXML
 	private ChoiceBox<String> stateChoiceBox;
-	@FXML
-	private ListView<Game> gameListView;
 	
 	private void loadGames(LogState state) {
 		ObservableList<Game> gamesList = FXCollections.observableList(gameDao.findAll());
-		gameListView.setItems(gamesList);
+		for (Game game : gamesList) {
+			loadGameGUI(game);
+		}
+	}
+	
+	private void loadGameGUI(Game game) {
+		ImageView iconImageView = new ImageView(this.getClass().getResource("assets/game.png").toExternalForm());
+		iconImageView.setFitWidth(32.0);
+		iconImageView.setFitHeight(32.0);
+		Label label = new Label(game.getName());
+		
+		AnchorPane anchorPane = new AnchorPane(iconImageView, label);
+		anchorPane.setBackground(new Background(new BackgroundFill(Paint.valueOf("#fff"), null, null)));
+		anchorPane.setPrefHeight(50);
+		
+		iconImageView.setLayoutX(15);
+		iconImageView.setLayoutY((anchorPane.getPrefHeight() - iconImageView.getFitHeight()) / 2);
+		
+		label.setFont(new Font(18));
+		label.setLayoutX(iconImageView.getLayoutX() + iconImageView.getFitWidth() + 15);
+		label.setLayoutY((50 - 30) / 2);
+		
+		mainVBox.getChildren().add(anchorPane);
 	}
 	
 	@Override
